9b8f9e3e19c7b89f198cd1b5807fe65cf5fc5396
JungYeon1220 <131150235+JungYeon1220@users.noreply.github.com>
2023-05-02 16:11:14 +0900

    DFS

--- 2303_WINAPI/2303_WINAPI/Object/Maze/Player.cpp
+++ 2303_WINAPI/2303_WINAPI/Object/Maze/Player.cpp
@@ -237,10 +237,10 @@ void Player::DFS(Vector2 here)
 {
 	Vector2 frontPos[4] =
 	{
-		Vector2 {-1, 0}, // LEFT
-		Vector2 {1, 0}, // RIGHT
 		Vector2 {0, -1}, // UP
-		Vector2 {0, 1} // DOWN
+		Vector2 {-1, 0}, // LEFT
+		Vector2 {0, 1}, // DOWN
+		Vector2 {1, 0} // RIGHT
 	};
 
 	_discovered[here.y][here.x] = true;
